6f6db0df74e7b18768fb875cdac7ab26695ec51c
AmiBell <miratou96@hotmail.com>
2018-02-20 13:33:18 +0100

    first commit

---
+++ app/src/main/java/com/example/aspirev/selki/Contactez_nous.java
@@ -0,0 +1,17 @@
+package com.example.aspirev.selki;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AspireV on 2018-02-20.
+ */
+
+public class Contactez_nous extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactez_nous);
+    }
+}
